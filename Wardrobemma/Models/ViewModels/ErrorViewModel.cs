61e10459a5773c8260cd3923902af01b72451694
Vicor Ungureanu <63206335+victorungureanu1005@users.noreply.github.com>
2022-10-05 20:55:07 +0300

    Migration to MVC, Navbar, HomePage

---
+++ Wardrobemma/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Wardrobemma.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -1,7 +1,4 @@
-using Microsoft.EntityFrameworkCore;
-using Wardrobemma.Data;
-
-namespace Wardrobemma
+namespace WebApplication2
 {
     public class Program
     {
@@ -10,18 +7,14 @@ namespace Wardrobemma
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddDbContext<WardrobeContext>(options => options.UseSqlServer(builder
-                .Configuration.GetConnectionString("DefaultConnection")));
-            builder.Services.AddRazorPages();
-            builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("Home/Error");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
